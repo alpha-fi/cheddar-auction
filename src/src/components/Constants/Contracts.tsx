3425ed7a156548527d5269be9903bcb2a951736c
marcel-krsh <marcel.krsh@gmail.com>
2022-07-03 21:06:41 -0400

    upgrade contract and ui

--- src/src/components/Constants/Contracts.tsx
+++ src/src/components/Constants/Contracts.tsx
@@ -1,2 +1,2 @@
 export const TENK_CONTRACT_ACCOUNT = "nft.cheddar.testnet"
-export const AUCTION_CONTRACT_ACCOUNT = "v2.test_cheddar_auction2.testnet"
+export const AUCTION_CONTRACT_ACCOUNT = "v4.test_cheddar_auction2.testnet"
